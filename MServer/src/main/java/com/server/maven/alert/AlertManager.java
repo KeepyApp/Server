397f7c98818ee88d2545d5a2c83b23590bfc8391
tairdahari <128617720+tairdahari@users.noreply.github.com>
2024-05-21 17:23:19 +0300

    TryToConnect

--- MServer/src/main/java/com/server/maven/alert/AlertManager.java
+++ MServer/src/main/java/com/server/maven/alert/AlertManager.java
@@ -1,7 +1,10 @@
 package com.server.maven.alert;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AlertManager {
     public void sendAlert(String recipient, String message) {
         // Send alert to the specified recipient through appropriate channel
+        System.out.println("Alert sent to " + recipient + ": " + message);
     }
 }
--- MServer/src/main/java/com/server/maven/microphoneDataHandler/MicrophoneDataHandler.java
+++ MServer/src/main/java/com/server/maven/microphoneDataHandler/MicrophoneDataHandler.java
@@ -1,24 +1,29 @@
 package com.server.maven.microphoneDataHandler;
 
+import com.server.maven.alert.AlertManager;
 import com.server.maven.analysis.AnalysisEngine;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class MicrophoneDataHandler {
     private AnalysisEngine analysisEngine;
+    private AlertManager alertManager;
 
-//    public MicrophoneDataHandler(AnalysisEngine analysisEngine) {
-//        this.analysisEngine = analysisEngine;
-//    }
+    public MicrophoneDataHandler(AnalysisEngine analysisEngine, AlertManager alertManager) {
+        this.analysisEngine = analysisEngine;
+        this.alertManager = alertManager;
+    }
 
     @PostMapping("/process-data")
     public void processData(@RequestBody String jsonData) {
         // Process the received JSON data here
         System.out.println("Received data from Python script: " + jsonData);
 
-        // Perform any necessary processing
+
+        alertManager.sendAlert("Taird","There is something unusual");
     }
     public void receiveAudioData(byte[] audioData) {
         // Receive raw audio data from Python component
